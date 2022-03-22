b92692af8e3a6431cb933de05bcce5205ae5e51e
Filip Starcevic <filip@prozor.org>
2022-03-22 16:00:22 +0100

    22.03.2022 dodao public

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const { rmSync } = require('fs')
 const http = require('http')
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'content_type':'text/plain'})
-    res.end('Hello World!')
+    const path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase()
+    switch(path){
+        case'':
+            res.writeHead(200, {'content-type':'text/plain'})
+            res.end('Homepage')
+            break
+        case'/about':
+            res.writeHead(200, {'content-type':'text/plain'})
+            res.end('Abaout')
+            break
+        default:
+            res.writeHead(404, {'content-type':'text/plain'})
+            res.end('Not Found')
+            break
+
+    }
 })
 
 server.listen(port, ()=> console.log(`server started on port ${port}; ` + 'press ctrl+C to get out ...'))
