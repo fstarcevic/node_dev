b581d35dce447bff7a4985e3e7d61ddd52c3251b
Filip Starcevic <filip@prozor.org>
2022-03-24 19:21:30 +0100

    24.03.2022 commit public

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-const { rmSync } = require('fs')
+const fs = require('fs')
 const http = require('http')
 const port = process.env.PORT || 3000
+
+function serveStaticFile(res, path, contentType, responsCode = 200){
+    fs.readFile(__dirname + path, (err, data) => {
+        if(err){
+            res.writeHead(500, {'content-type':'text/plain'})
+            return res.end('500 - Internal Error')
+        }
+        res.writeHead(responsCode, {'content-type': contentType})
+        res.end(data)
+    })
+}
 
 const server = http.createServer((req, res) => {
     const path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase()
     switch(path){
         case'':
-            res.writeHead(200, {'content-type':'text/plain'})
-            res.end('Homepage')
+            serveStaticFile(res, '/public/home.html', 'text/html')
             break
         case'/about':
-            res.writeHead(200, {'content-type':'text/plain'})
-            res.end('Abaout')
+            serveStaticFile(res, 'public/about.html', 'text/html')
+            break
+        case'/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png')
             break
         default:
-            res.writeHead(404, {'content-type':'text/plain'})
-            res.end('Not Found')
+            serveStaticFile(res, '/public/404.html', 'text/html', 404)
             break
-
     }
 })
 
