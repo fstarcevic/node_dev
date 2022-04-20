aabcf322922a9bb9b370343fcea92dde67353e12
Filip Starcevic <filip@prozor.org>
2022-04-20 13:43:50 +0200

    20.04.2022 templates

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 const express = require('express')
+const expresshandlebars = require('express-handlebars')
 
+/*const mysql = require('mysql')
+const mysqldevapi = require('@mysqldevapi')
+
+connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308,
+    user: 'root',
+    password: 'Trek5200',
+    database: 'userprice'
+})
+*/
 const app = express()
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Medowlark Travel')
-})
+app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('About medowlark Travel')
-})
+app.get('/about', (req, res) => res.render('about'))
 
 app.use((req, res) => {
     res.type('text/plain')
     res.status(404)
-    res.send('404 - not found')
+    res.render('404')
 })
 
 app.use((err, res, req, next) => {
     console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen (port, () => console.log ('express started on server ' + 'pres ctrl-c for exit'))
