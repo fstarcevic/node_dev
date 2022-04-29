dd1f3045c356fb5adce2c0551aa9ee79b5b16fc1
Filip Starcevic <filip@prozor.org>
2022-04-29 14:07:56 +0200

    commit 29.04.2022

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 3000;
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+]
+
 app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => res.render('about'));
+app.get('/about', (req, res) => {
+    
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune: randomFortune})
+
+});
 
 app.use((req, res) => {
     res.type('text/plain');
@@ -29,5 +42,7 @@ app.use((err, res, req, next) => {
 });
 
 app.use(express.static(__dirname + '/public'))
+
+
 
 app.listen (port, () => console.log ('express started on server ' + 'pres ctrl-c for exit'));
